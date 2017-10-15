1131be68eb2785b4a1beeed19f55286855954df6
severjason <ssever2010@gmail.com>
2017-10-16 01:42:33 +0300

    Home component update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,38 @@
 import {Component, OnInit} from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import {ApiService} from '../api/api.service';
+import {IProducts} from '../api/api.interface';
 
 @Component({
   templateUrl: './home.component.html',
+  providers: [ApiService],
 })
 export class HomeComponent implements OnInit {
 
-  private title = 'All products';
+  private _title = 'All products';
+  public products: IProducts[];
+  public error: string;
 
-  constructor(private titleService: Title) {}
+  constructor(private titleService: Title, private apiService: ApiService) {}
 
   ngOnInit() {
-    this.titleService.setTitle(this.title);
+    this.titleService.setTitle(this._title);
+    this.getProducts();
   }
+
+  public getProducts(): IProducts[] | void {
+    this.apiService.getProducts()
+      .subscribe(
+        res => {
+          this.products = res;
+          console.log(this.products);
+        },
+        error => {
+          this.error = error;
+          console.log('Error :: ' + error)
+        }
+      )
+  }
+
+
 }
