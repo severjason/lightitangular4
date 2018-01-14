3fcfccaddf49d7ecf7c86d0d8d2078b453a8eab3
severjason <ssever2010@gmail.com>
2018-01-14 21:21:04 +0200

    Updated product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import {Title} from '@angular/platform-browser';
+import {ActivatedRoute, Router} from '@angular/router';
 import {ApiService} from '../api/api.service';
+import {IProduct} from '../api/api.interface';
 
 @Component({
   templateUrl: './product.component.html',
@@ -9,11 +11,26 @@ import {ApiService} from '../api/api.service';
 
 export class ProductComponent implements OnInit {
 
-  private _title = 'All products';
+  private _pageTitle = 'Product | ';
+  private _id = 0;
+  private _products: IProduct[];
+  public product: any;
 
-  constructor(private titleService: Title, private apiService: ApiService) {}
+  constructor(
+    private titleService: Title,
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private router: Router) {
+    this._id = +this.route.snapshot.paramMap.get('id') - 1;
+    this._products = this.route.snapshot.data.products;
+  }
 
   ngOnInit() {
-    this.titleService.setTitle(this._title);
+    this.titleService.setTitle(this._pageTitle + this._id);
+    if (!this._products[this._id]) {
+      this.router.navigate(['all-products']);
+    }
+    this.product = this._products[this._id];
+    console.log(this.product);
   }
 }
