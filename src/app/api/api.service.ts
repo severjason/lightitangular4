89c8d4e9a5f5f12b7e94006cf566127cc2f22769
severjason <ssever2010@gmail.com>
2018-01-18 23:14:45 +0200

    Added register, login and getReviews methods

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,20 +1,24 @@
-import {Injectable} from '@angular/core';
+import {Injectable, SecurityContext} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
 import {IProduct} from './api.interface'
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Injectable()
 export class ApiService {
 
   private _apiUrl = 'http://smktesting.herokuapp.com/api/';
   private _productsUrl = 'products/';
+  private _registerUrl = 'register/';
+  private _loginUrl = 'login/';
+  private _reviewsUrl = 'reviews/';
 
   static handleError(error: Response) {
     return Observable.throw(error.statusText);
   }
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private sanitizer: DomSanitizer) {
   }
 
   public getProducts(): Observable<IProduct[]> {
@@ -22,6 +26,28 @@ export class ApiService {
       .get(this._apiUrl + this._productsUrl)
       .map((response: Response) => {
         return <IProduct[]>response.json();
+      })
+      .catch(ApiService.handleError);
+  }
+
+  public register(newUser: any): Observable<any> {
+    return this.http.post(this._apiUrl + this._registerUrl, {
+        username: this.sanitizer.sanitize(SecurityContext.HTML, newUser.username),
+        password: newUser.password,
+      })
+  }
+
+  public login(user: any): Observable<any> {
+    return this.http.post(this._apiUrl + this._loginUrl, {
+      username: this.sanitizer.sanitize(SecurityContext.HTML, user.username),
+      password: user.password,
+    })
+  }
+
+  public getReviews(productId: number): Observable<any> {
+    return this.http.get(this._apiUrl + this._reviewsUrl + `${productId}`)
+      .map((response: Response) => {
+        return response.json();
       })
       .catch(ApiService.handleError);
   }
