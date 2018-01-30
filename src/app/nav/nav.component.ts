0044139d2bdb0850fe6cc7c42e7f2115ea7a1c5d
severjason <ssever2010@gmail.com>
2018-01-30 16:50:21 +0200

    Created nav component

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+})
+
+export class NavComponent implements OnInit {
+
+  constructor(private auth: AuthService) {
+  }
+
+  ngOnInit() {
+    this.loggedIn()
+  }
+
+  public onLogout(): void {
+    this.auth.logout();
+  }
+
+  public loggedIn(): boolean {
+    return this.auth.loggedIn();
+  }
+
+  /*
+  public getUsername(): string | boolean {
+    return this.auth.getUserName();
+  }*/
+}
