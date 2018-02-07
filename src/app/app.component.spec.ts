3bd67f63c4d0b21c1e2fb95e33df6ac55bb531be
severjason <ssever2010@gmail.com>
2018-02-07 15:36:14 +0200

    Small fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,23 @@
 import {TestBed, async, fakeAsync} from '@angular/core/testing';
 
 import {AppComponent} from './app.component';
+import {BrowserModule, Title} from '@angular/platform-browser';
+import {ApiService} from './services/api.service';
+import {AuthGuardService} from './services/authguard.service';
+import {HomeComponent} from './home/home.component';
+import {SignUpComponent} from './signUp/signUp.component';
+import {LoginComponent} from './login/login.component';
+import {ProductComponent} from './product/product.component';
+import {OrderByPipe} from './product/orderby.pipe';
+import {AuthService} from './services/auth.service';
+import {CookieService} from 'ngx-cookie-service';
+import {AppCookieService} from './services/cookie.service';
+import {RateClassPipe} from './product/product-rate.pipe';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NavComponent} from './nav/nav.component';
+import {ProductResolver} from './product/products.resolver';
+import {RouterTestingModule} from '@angular/router/testing';
 
 
 describe('AppComponent', () => {
@@ -8,26 +25,35 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
+        NavComponent,
+        HomeComponent,
+        ProductComponent,
+        SignUpComponent,
+        LoginComponent,
+        RateClassPipe,
+        OrderByPipe,
+      ],
+      imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        Title,
+        ApiService,
+        ProductResolver,
+        AuthService,
+        CookieService,
+        AppCookieService,
+        AuthGuardService,
       ],
     }).compileComponents();
   }));
-/*
-  it('should create the app', fakeAsync(() => {
+
+  it('Should create the app', fakeAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));*/
-/*
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
   }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));*/
 });
